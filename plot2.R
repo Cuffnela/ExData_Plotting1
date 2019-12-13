5a9afe4364a7bc38b4f04d8ee52b37ab0e0d52f7
Cuffnela <cuffnela@gmail.com>
2019-12-12 20:44:23 -0500

    initial plot 2 commit removed unused code from plot1.R

--- plot2.R
+++ plot2.R
@@ -9,20 +9,23 @@
 # read data in from file
 ################################################################################
 data<-read.table("household_power_consumption.txt",
-               colClasses="character",sep=";",header=FALSE, skip = 66637, 
-               nrows = 2879)
+                 colClasses="character",sep=";",header=FALSE, skip = 66637, 
+                 nrows = 2879)
 vars<-read.table("household_power_consumption.txt",
                  colClasses="character",sep=";",header=FALSE, nrows = 1)
 names(data)<-vars
+
+# create combined Datetime variable
+data$DateTime<-paste(data$Date, data$Time)
+# convert Date and Time variables to Date/Time classes
+data$DateTime<-strptime(data$DateTime, format = "%d/%m/%Y %H:%M:%S")
 ################################################################################
 
-# Requirments
-# PNG file 480px by 480px
-# Plot1 Global Active Power (kilowatts) histogram with red bars
-
+# Requirements
+# png 480px by 480px
+# Day of the Week vs. Global Active Power
 #open png graphics device
-png(filename = "plot1.png")
-with(data, hist(as.numeric(Global_active_power), col="red", 
-                xlab = "Global Active Power (kilowatts)",
-                main="Global Active Power"))
+png(filename = "plot2.png")
+plot(data$DateTime, data$Global_active_power, type="l", 
+     ylab = "Global Active Power (kilowatts)", xlab="")
 dev.off()
